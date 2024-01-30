92dd54e813c5fce2995bbe6017fa17085e977351
NathanCo2 <156120309+NathanCo2@users.noreply.github.com>
2024-01-30 08:58:45 -0800

    led sturggle

--- src/main.py
+++ src/main.py
@@ -12,14 +12,15 @@ https://matplotlib.org/stable/gallery/user_interfaces/embedding_in_tk_sgskip.htm
 @date   2024-01-30 Original program, based on example from above listed source
 @copyright (c) 2024 by Jessica Perez, Jacquelyn Banh, and Nathan Chapman and released under the GNU Public Licenes V3
 """
+import time
 
 def led_setup(): 
     """!
-    Doxygen style docstring for interrupt callback function
+    Sets up LED
     """
-    val = adcpin.read()
-    int_queue.put(val)# Puts an integer into the queue
-    
+    # Initialize pins/ADC Objects (aka initialize pins)
+    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)#Configures pinA0 for Output
+    return pinA0
 def led_brightness(): 
     """!
     Doxygen style docstring for interrupt callback function
@@ -27,6 +28,11 @@ def led_brightness():
     val = adcpin.read()
     int_queue.put(val)# Puts an integer into the queue
 
-
+# This main code is run if this file is the main program but won't run if this
+# file is imported as a module by some other main program
 if __name__ == "__main__":
-    step_response(timer_int)
+    pinA0 = led_setup()
+    while True:
+        pinA0.low
+        time.sleep(1)
+        pinA0.high
