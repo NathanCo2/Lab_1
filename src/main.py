29b2fb95339fb97123a7738b3419a910f9e3be0a
jrbanh <156119463+jrbanh@users.noreply.github.com>
2024-01-30 09:44:33 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,27 +13,39 @@ https://matplotlib.org/stable/gallery/user_interfaces/embedding_in_tk_sgskip.htm
 @copyright (c) 2024 by Jessica Perez, Jacquelyn Banh, and Nathan Chapman and released under the GNU Public Licenes V3
 """
 import time
+import pyb
+import micropython
+
+# Initialize pins/ADC Objects (aka initialize pins)
+    # Configure Pin A0 for output
+pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+    # Configure PWM3/2 for timer (Timer 3 and Channel 2)
+pin32 = pyb.Pin(pyb.Pin.board.D4, pyb.Pin.OUT_PP)
 
 def led_setup(): 
     """!
     Sets up LED
     """
-    # Initialize pins/ADC Objects (aka initialize pins)
-    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP) #Configures pinA0 for Output
-    return pinA0
+    tim_num = pyb.Timer.PWM_INVERTED(2) #Creates a timer 
+    tim_num.init(freq=1000) #Starts the timer
+    #ch.pulse_width_percent(50)
+    
+
 def led_brightness(): 
     """!
     Doxygen style docstring for interrupt callback function
     """
-    val = adcpin.read()
-    int_queue.put(val)# Puts an integer into the queue
-
-# This main code is run if this file is the main program but won't run if this
-# file is imported as a module by some other main program
-if __name__ == "__main__":
-    pinA0 = led_setup()
     while True:
         time.sleep(0.5)
         pinA0.low()
         time.sleep(0.5)
         pinA0.high()
+
+    
+# This main code is run if this file is the main program but won't run if this
+# file is imported as a module by some other main program
+if __name__ == "__main__":
+    led_brightness(led_setup)
+    
+    #pinA0 = led_setup()
+
