ceb711993ef6eeb91b681b4a01e2c6a1292cf493
jrbanh <156119463+jrbanh@users.noreply.github.com>
2024-01-30 10:00:03 -0800

    Update led.py

--- src/led.py
+++ src/led.py
@@ -18,30 +18,34 @@ import micropython
 
 # Initialize pins/ADC Objects (aka initialize pins)
     # Configure Pin A0 for output and timer (PWM2/1: Timer 2 and Channel 1)
-pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+pinA0 = pyb.Pin(pyb.Pin.board.PA0)
+#pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
 
 def led_setup(): 
     """!
     Sets up LED
     """
-    tim_num = pyb.Timer.PWM_INVERTED(2) #Creates a timer 
-    tim_num.init(freq=1000) #Starts the timer
-    
-def led_brightness(): 
+    #tim_num = pyb.Timer.PWM_INVERTED #Creates a timer 
+    #tim_num.init(freq=1000) #Starts the timer
+    tim = pyb.Timer(2, freq=1000)  # Timer 2, frequency 1000Hz
+    # Configure PWM channel with inverted polarity
+    ch = tim.channel(1, pyb.Timer.PWM_INVERTED, pin=pinA0)  # PWM on Timer 2, Channel 1
+    return ch
+
+def led_brightness(ch, duty_cycle): 
     """!
     Doxygen style docstring for interrupt callback function
     """
-    
+    ch.pulse_width_percent = (int(duty_cycle * 100))
 
 
-    
 # This main code is run if this file is the main program but won't run if this
 # file is imported as a module by some other main program
 if __name__ == "__main__":
-    led_brightness(led_setup)
+    ch = led_setup()
     while True:
-        time.sleep(0.5)
-        pinA0.low()
-        time.sleep(0.5)
-        pinA0.high()
+        time.sleep(1)
+        led_brightness(ch, 0.5)
+        time.sleep(1)
+        led_brightness(ch, 0.8)
 
